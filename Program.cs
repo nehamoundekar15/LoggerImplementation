47e3e6cbdc40afea5a7997e5f201dee42287e55e
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-07-16 15:38:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace LoggerImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("**************************************************");
+            try
+            {
+                throw new Exception("This is an Exception");
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+                LogExceptionToFile(ex);
+                
+                Console.ReadKey();  
+            }
+        }
+
+        private static void LogExceptionToFile(Exception ex)
+        {
+            string filePath = "logger.txt";
+            string logMessage = $"[{DateTime.Now}] Exception: {ex.Message}{Environment.NewLine}{ex.StackTrace}{Environment.NewLine}";
+            File.AppendAllText(filePath, logMessage);
+        }
+    }
+}
+ 
